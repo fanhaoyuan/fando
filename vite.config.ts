56598b146003838f145a880eec96ddc256d3a3e8
fansy <418536538@qq.com>
2022-06-12 23:13:40 +0800

    style: add css module config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const scopedNameMap = new Map();
+
 export default async () => {
     const mdx = (await import('@mdx-js/rollup')).default;
 
     return defineConfig({
         plugins: [react(), mdx()],
+        css: {
+            modules: {
+                localsConvention: 'camelCaseOnly',
+                generateScopedName(name, fileName) {
+                    if (!scopedNameMap.has(fileName)) {
+                        scopedNameMap.set(fileName, Math.random().toString(36).slice(-6));
+                    }
+
+                    if (/^r-ant.*/.test(name)) {
+                        return name;
+                    }
+
+                    return `${name}__${scopedNameMap.get(fileName)}`;
+                },
+            },
+        },
     });
 };
