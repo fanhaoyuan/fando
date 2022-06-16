20c1a8f01856d9164bd180f107d2c0eff52710cc
fanhaoyuan <418536538@qq.com>
2022-06-16 17:38:46 +0800

    feat: add `defineConfig` types helpers

---
+++ lib/defineConfig.ts
@@ -0,0 +1,10 @@
+import { Config } from './interfaces';
+
+/**
+ * Config Helpers
+ * @param config
+ * @returns
+ */
+export function defineConfig(config: Config): Config {
+    return config;
+}
---
+++ lib/interfaces.ts
@@ -0,0 +1,22 @@
+export interface Config {
+    /**
+     * Configure the name of the document on the header
+     * 
+     * @default `require('package.json').name`
+     */
+    title?: string;
+
+    /**
+     * To set the LOGO of the document.
+     */
+    logo?: string;
+    
+    /**
+     * The path for mdx files to search
+     *
+     * Relative to `process.cwd()`
+     *
+     * @default 'docs/src'
+     */
+    docsPath?: string;
+}
